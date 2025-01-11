6826cb6e2aae1b3bfcac48c73b146d5b009f83cf
Gregory Edison <gregory.edison1993@gmail.com>
2025-01-11 15:10:15 +0100

    feat: remove code_size access in Host trait

--- crates/interpreter/src/host.rs
+++ crates/interpreter/src/host.rs
@@ -48,10 +48,6 @@ pub trait Host {
     /// Get code hash of `address` and if the account is cold.
     fn code_hash(&mut self, address: Address) -> Option<StateLoad<B256>>;
 
-    #[cfg(feature = "scroll")]
-    /// Get code size of `address` and if the account is cold.
-    fn code_size(&mut self, address: Address) -> Option<(usize, bool)>;
-
     /// Get storage value of `address` at `index` and if the account is cold.
     fn sload(&mut self, address: Address, index: U256) -> Option<StateLoad<U256>>;
 
