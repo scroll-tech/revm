47ce66dcdf52baadd86233453c62f59b9d6d803b
lightsing <light.tsing@gmail.com>
2025-02-12 19:17:42 +0800

    add apply_eip7702_auth_list handler

--- crates/revm/src/scroll.rs
+++ crates/revm/src/scroll.rs
@@ -2,7 +2,7 @@ mod handler_register;
 mod l1block;
 
 pub use crate::scroll::handler_register::{
-    deduct_caller, load_accounts, reward_beneficiary, scroll_handle_register,
-    validate_tx_against_state,
+    apply_eip7702_auth_list, deduct_caller, load_accounts, reward_beneficiary,
+    scroll_handle_register, validate_tx_against_state,
 };
 pub use crate::scroll::l1block::{L1BlockInfo, L1_GAS_PRICE_ORACLE_ADDRESS};
