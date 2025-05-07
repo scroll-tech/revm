ecba09736e58f7f0e3f263677f691d5d9bfe5923
Gregory Edison <gregory.edison1993@gmail.com>
2025-05-07 16:37:45 +0200

    test: fix integration

--- crates/op-revm/tests/integration.rs
+++ crates/op-revm/tests/integration.rs
@@ -90,9 +90,10 @@ fn p256verify_test_tx(
 ) -> Context<BlockEnv, OpTransaction<TxEnv>, CfgEnv<OpSpecId>, EmptyDB, Journal<EmptyDB>, L1BlockInfo>
 {
     const SPEC_ID: OpSpecId = OpSpecId::FJORD;
+    let is_eip7702_enabled = SPEC_ID >= OpSpecId::ISTHMUS;
 
     let InitialAndFloorGas { initial_gas, .. } =
-        calculate_initial_tx_gas(SPEC_ID.into(), &[], false, 0, 0, 0);
+        calculate_initial_tx_gas(SPEC_ID.into(), &[], false, is_eip7702_enabled, 0, 0, 0);
 
     Context::op()
         .modify_tx_chained(|tx| {
@@ -138,9 +139,10 @@ fn bn128_pair_test_tx(
     spec: OpSpecId,
 ) -> Context<BlockEnv, OpTransaction<TxEnv>, CfgEnv<OpSpecId>, EmptyDB, Journal<EmptyDB>, L1BlockInfo>
 {
+    let is_eip7702_enabled = spec >= OpSpecId::ISTHMUS;
     let input = Bytes::from([1; GRANITE_MAX_INPUT_SIZE + 2]);
     let InitialAndFloorGas { initial_gas, .. } =
-        calculate_initial_tx_gas(spec.into(), &input[..], false, 0, 0, 0);
+        calculate_initial_tx_gas(spec.into(), &input[..], false, is_eip7702_enabled, 0, 0, 0);
 
     Context::op()
         .modify_tx_chained(|tx| {
@@ -256,10 +258,18 @@ fn g1_msm_test_tx(
 ) -> Context<BlockEnv, OpTransaction<TxEnv>, CfgEnv<OpSpecId>, EmptyDB, Journal<EmptyDB>, L1BlockInfo>
 {
     const SPEC_ID: OpSpecId = OpSpecId::ISTHMUS;
+    let is_eip7702_enabled = SPEC_ID >= OpSpecId::ISTHMUS;
 
     let input = Bytes::from([1; bls12_381_const::G1_MSM_INPUT_LENGTH]);
-    let InitialAndFloorGas { initial_gas, .. } =
-        calculate_initial_tx_gas(SPEC_ID.into(), &input[..], false, 0, 0, 0);
+    let InitialAndFloorGas { initial_gas, .. } = calculate_initial_tx_gas(
+        SPEC_ID.into(),
+        &input[..],
+        false,
+        is_eip7702_enabled,
+        0,
+        0,
+        0,
+    );
     let gs1_msm_gas = bls12_381_utils::msm_required_gas(
         1,
         &bls12_381_const::DISCOUNT_TABLE_G1_MSM,
@@ -413,10 +423,18 @@ fn g2_msm_test_tx(
 ) -> Context<BlockEnv, OpTransaction<TxEnv>, CfgEnv<OpSpecId>, EmptyDB, Journal<EmptyDB>, L1BlockInfo>
 {
     const SPEC_ID: OpSpecId = OpSpecId::ISTHMUS;
+    let is_eip7702_enabled = SPEC_ID >= OpSpecId::ISTHMUS;
 
     let input = Bytes::from([1; bls12_381_const::G2_MSM_INPUT_LENGTH]);
-    let InitialAndFloorGas { initial_gas, .. } =
-        calculate_initial_tx_gas(SPEC_ID.into(), &input[..], false, 0, 0, 0);
+    let InitialAndFloorGas { initial_gas, .. } = calculate_initial_tx_gas(
+        SPEC_ID.into(),
+        &input[..],
+        false,
+        is_eip7702_enabled,
+        0,
+        0,
+        0,
+    );
     let gs2_msm_gas = bls12_381_utils::msm_required_gas(
         1,
         &bls12_381_const::DISCOUNT_TABLE_G2_MSM,
@@ -506,10 +524,18 @@ fn bl12_381_pairing_test_tx(
 ) -> Context<BlockEnv, OpTransaction<TxEnv>, CfgEnv<OpSpecId>, EmptyDB, Journal<EmptyDB>, L1BlockInfo>
 {
     const SPEC_ID: OpSpecId = OpSpecId::ISTHMUS;
+    let is_eip7702_enabled = SPEC_ID >= OpSpecId::ISTHMUS;
 
     let input = Bytes::from([1; bls12_381_const::PAIRING_INPUT_LENGTH]);
-    let InitialAndFloorGas { initial_gas, .. } =
-        calculate_initial_tx_gas(SPEC_ID.into(), &input[..], false, 0, 0, 0);
+    let InitialAndFloorGas { initial_gas, .. } = calculate_initial_tx_gas(
+        SPEC_ID.into(),
+        &input[..],
+        false,
+        is_eip7702_enabled,
+        0,
+        0,
+        0,
+    );
 
     let pairing_gas: u64 =
         bls12_381_const::PAIRING_MULTIPLIER_BASE + bls12_381_const::PAIRING_OFFSET_BASE;
@@ -598,10 +624,18 @@ fn fp_to_g1_test_tx(
 ) -> Context<BlockEnv, OpTransaction<TxEnv>, CfgEnv<OpSpecId>, EmptyDB, Journal<EmptyDB>, L1BlockInfo>
 {
     const SPEC_ID: OpSpecId = OpSpecId::ISTHMUS;
+    let is_eip7702_enabled = SPEC_ID >= OpSpecId::ISTHMUS;
 
     let input = Bytes::from([1; bls12_381_const::PADDED_FP_LENGTH]);
-    let InitialAndFloorGas { initial_gas, .. } =
-        calculate_initial_tx_gas(SPEC_ID.into(), &input[..], false, 0, 0, 0);
+    let InitialAndFloorGas { initial_gas, .. } = calculate_initial_tx_gas(
+        SPEC_ID.into(),
+        &input[..],
+        false,
+        is_eip7702_enabled,
+        0,
+        0,
+        0,
+    );
 
     Context::op()
         .modify_tx_chained(|tx| {
@@ -664,10 +698,18 @@ fn fp2_to_g2_test_tx(
 ) -> Context<BlockEnv, OpTransaction<TxEnv>, CfgEnv<OpSpecId>, EmptyDB, Journal<EmptyDB>, L1BlockInfo>
 {
     const SPEC_ID: OpSpecId = OpSpecId::ISTHMUS;
+    let is_eip7702_enabled = SPEC_ID >= OpSpecId::ISTHMUS;
 
     let input = Bytes::from([1; bls12_381_const::PADDED_FP2_LENGTH]);
-    let InitialAndFloorGas { initial_gas, .. } =
-        calculate_initial_tx_gas(SPEC_ID.into(), &input[..], false, 0, 0, 0);
+    let InitialAndFloorGas { initial_gas, .. } = calculate_initial_tx_gas(
+        SPEC_ID.into(),
+        &input[..],
+        false,
+        is_eip7702_enabled,
+        0,
+        0,
+        0,
+    );
 
     Context::op()
         .modify_tx_chained(|tx| {
